5a36531afcc4b4af0cc9bc6ceb619a43dafd9282
BohaLash <dogdanlashko@gmail.com>
2020-07-20 16:10:48 +0300

    😜

--- js/main.js
+++ js/main.js
@@ -1,23 +1,42 @@
-// The Best CRM Ever
+// Neomorphic Clock
 
-class CardMeneger {
-    constructor(table) {
-        this.opened = false
-        table.find("tr").click((e) => this.rowClicked(e));
-        $("body").click((e) => this.close());
-        $(".card").click(e => e.stopPropagation())
+// function toggleTheme() {
+//     if (document.body.className == 'th1') {
+//         document.body.className = 'th2';
+//     } else {
+//         document.body.className = 'th1';
+//     }
+// }
+
+// document.getElementById('theme').onclick = toggleTheme;
+
+
+
+var clock = document.getElementById('border'), k = 180 / Math.PI, deg;
+
+var h = document.getElementById('h');
+var m = document.getElementById('m');
+var s = document.getElementById('s');
+
+document.onmousemove = handleMouseMove;
+
+function handleMouseMove(event) {
+    deg = Math.atan((event.pageX - window.innerWidth / 2) / Math.abs(event.pageY - window.innerHeight / 2)) * k;
+
+    if (event.pageY > window.innerHeight / 2) {
+        deg *= -1;
+        deg += 180;
     }
-    rowClicked(e) {
-        if (this.opened == false) {
-            $('.card').css("display", "block")
-            this.opened = true;
-            e.stopPropagation()
-        }
-    }
-    close() {
-        $('.card').css("display", "none")
-        this.opened = false;
-    }
+
+    // console.log(deg);
+
+    clock.style.transform = 'translate(-50%, -50%) rotate(' + deg + 'deg)'; 
 }
 
-var Cards = new CardMeneger($('.result>table'))
+setInterval(() => {
+    let day = new Date();
+
+    h.style.transform = 'translate(-50%, -50%) rotate(' + (day.getHours() % 12) * 30 + 'deg)';
+    m.style.transform = 'translate(-50%, -50%) rotate(' + day.getMinutes() * 6 + 'deg)';
+    s.style.transform = 'translate(-50%, -50%) rotate(' + day.getSeconds() * 6 + 'deg)';
+})
