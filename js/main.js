1be7ec7e8ead2585a5cfe0248a1733b6592c5cb8
BohaLash <dogdanlashko@gmail.com>
2020-07-20 17:23:18 +0300

    😜

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 // Neomorphic Clock
 
-// function toggleTheme() {
-//     if (document.body.className == 'th1') {
-//         document.body.className = 'th2';
-//     } else {
-//         document.body.className = 'th1';
-//     }
-// }
+function toggleTheme() {
+    if (document.body.className == 'th1') {
+        document.body.className = 'th2';
+    } else {
+        document.body.className = 'th1';
+    }
+}
 
-// document.getElementById('theme').onclick = toggleTheme;
+document.onclick = toggleTheme;
 
 
 
